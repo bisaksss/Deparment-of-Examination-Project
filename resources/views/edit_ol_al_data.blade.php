d4e54ebb3aa9d7f6675484a74589991c5b94316e
bisaksss <bisaksss@gmail.com>
2021-06-22 22:43:59 +0530

    Add headers

--- resources/views/edit_ol_al_data.blade.php
+++ resources/views/edit_ol_al_data.blade.php
@@ -12,7 +12,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>O/L paper bundle</title>
+    <title>Edit O/L||A/L Paper Bundle</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
    <style>
